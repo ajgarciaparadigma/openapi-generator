c1490b948816ae975f69f38c1a7877575efe0b21
ajgarcia <ajgarcia@paradigmadigital.com>
2023-06-25 14:59:18 +0200

    initial

---
+++ src/main/java/com/paradigma/serveropenapigenerator/ServerOpenapiGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.paradigma.serveropenapigenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerOpenapiGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerOpenapiGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paradigma/serveropenapigenerator/ServerOpenapiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradigma.serveropenapigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerOpenapiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
